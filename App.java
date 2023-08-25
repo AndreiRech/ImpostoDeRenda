d52370b68e342e0d96d22bfa51d9002f6bbfb02c
Andrei <andreirech22@outlook.com>
2023-08-25 18:42:13 -0300

    Reformulando a main

--- App.java
+++ App.java
@@ -1,34 +1,96 @@
-<<<<<<< HEAD
 public class App {
     private static Scanner in = new Scanner (System.in);
-
     public static void main(String args[]) {
-
-        System.out.println("Deseja fazer:\n1-Declaração completa\n2-Declaração simplificada");
-        int opcao in.nextInt();
-        switch (opcao) {
-            case 1:
-                System.out.println("Informe seu nome: ");
-                String nome = in.nextLine();
-                System.out.println("Informe seu CPF: ");
-                int cpf = in.nextInt();
-                System.out.println("Informe sua idade: ");
-                int idade = in.nextInt();
-                System.out.println("Informe o número de dependentes: ");
-                int dependentes = in.nextInt();
-                System.out.println("Informe sua contribuição previdenciária oficial: ");
-                int contribuicao = in.nextInt();
-                System.out.println("Informe seu total de rendimentos");
-                int rendimentos = in.nextInt();
-            case 2:
-                String nome = in.nextLine();
-                System.out.println("Informe seu CPF: ");
-                int cpf = in.nextInt();
-                System.out.println("Informe sua contribuição previdenciária oficial: ");
-                int contribuicao = in.nextInt();
-                System.out.println("Informe seu total de rendimentos");
-                int rendimentos = in.nextInt();
-        }
+        opcao();
     }
 
+    public static void menu () {
+        String mensagem = """
+
+                ------------------- [ MENU ] -------------------
+
+                Informe o que deseja fazer:
+
+                1. Realizar declaração simplificada;
+                2. Realizar declaração completa;
+                3. Verificar o imposto a pagar;
+                4. Sair.
+
+                """;
+        System.out.println(mensagem);
+    }
+
+    public static void opcao () {
+        int opc = 0;
+
+        do {
+            menu();
+            opc = in.nextInt();
+
+            switch (opc) {
+                case 1:
+                    decSimplificada();
+                    break;
+                case 2:
+                    decCompleta();
+                    break;
+                case 3:
+                    recebeImposto();
+                    break;
+                case 4:
+                    System.out.println("\nSaindo do aplicativo...");
+                    break;
+                default:
+                    System.out.println("\nInforme uma opção válida!");
+                    break;
+            }
+        }
+        while (opc != 3);
+    }
+
+    public static void decCompleta () {
+        String nome;
+        int cpf, idade, dependentes;
+        double contribuicao, rendimentos;
+
+        System.out.println("Informe seu nome: ");
+        nome = in.nextLine();
+
+        System.out.println("Informe seu CPF: ");
+        cpf = in.nextInt();
+
+        System.out.println("Informe sua idade: ");
+        idade = in.nextInt();
+
+        System.out.println("Informe o número de dependentes: ");
+        dependentes = in.nextInt();
+
+        System.out.println("Informe sua contribuição previdenciária oficial: ");
+        contribuicao = in.nextDouble();
+
+        System.out.println("Informe seu total de rendimentos");
+        rendimentos = in.nextDouble();
+
+        calculoDaBase(contribuicao, rendimentos);
+    }
+
+    public static void decSimplificada () {
+        String nome;
+        int cpf;
+        double contribuicao, rendimentos;
+
+        System.out.println("Informe seu nome: ");
+        nome = in.nextLine();
+
+        System.out.println("Informe seu CPF: ");
+        cpf = in.nextInt();
+
+        System.out.println("Informe sua contribuição previdenciária oficial: ");
+        contribuicao = in.nextDouble();
+
+        System.out.println("Informe seu total de rendimentos");
+        rendimentos = in.nextDouble();
+
+        calculoDaBase(contribuicao, rendimentos);
+    }
 }
